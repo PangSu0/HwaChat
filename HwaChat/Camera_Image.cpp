a26690cf1c2936a1b7c15cb45324c72e9c98b332
ina <bina1796@gmail.com>
2019-07-09 18:54:30 +0900

    winform에서 영상 띄우기

---
+++ HwaChat/Camera_Image.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <opencv2\opencv.hpp>
+#include "Header.h"
+
+using namespace cv;
+using namespace std;
+using namespace defCamera;
+
+camera::camera()
+{
+}
+System::Drawing::Bitmap^ camera::Show(cv::Mat& colorImage) {
+	System::IntPtr ptr(colorImage.ptr());
+	System::Drawing::Bitmap^ b;
+	switch (colorImage.type())
+	{
+	case CV_8UC3:
+		b = gcnew System::Drawing::Bitmap(colorImage.cols, colorImage.rows, colorImage.step,
+			System::Drawing::Imaging::PixelFormat::Format24bppRgb, ptr);
+		break;
+	case CV_8UC1:
+		b = gcnew System::Drawing::Bitmap(colorImage.cols, colorImage.rows, colorImage.step,
+			System::Drawing::Imaging::PixelFormat::Format8bppIndexed, ptr);
+		break;
+	default:
+		break;
+	}
+	return b;
+}
--- HwaChat/MyForm.h
+++ HwaChat/MyForm.h
@@ -3,10 +3,12 @@
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include "Header.h"
 
 namespace HwaChat {
 
@@ -18,8 +20,7 @@ namespace HwaChat {
 	using namespace System::Drawing;
 	using namespace cv;
 	using namespace std; 
-
-	int showMyCamera();
+	using namespace defCamera;
 
 	/// <summary>
 	/// Summary for MyForm
@@ -48,6 +49,8 @@ namespace HwaChat {
 		}
 	private: System::Windows::Forms::Button^ btnMyCameraON;
 	private: System::Windows::Forms::Button^ btnMyCameraOFF;
+	private: System::Windows::Forms::PictureBox^ picCam;
+	private: System::Windows::Forms::Button^ btnPlay;
 	protected:
 
 
@@ -78,11 +81,14 @@ namespace HwaChat {
 		{
 			this->btnMyCameraON = (gcnew System::Windows::Forms::Button());
 			this->btnMyCameraOFF = (gcnew System::Windows::Forms::Button());
+			this->picCam = (gcnew System::Windows::Forms::PictureBox());
+			this->btnPlay = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picCam))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// btnMyCameraON
 			// 
-			this->btnMyCameraON->Location = System::Drawing::Point(26, 105);
+			this->btnMyCameraON->Location = System::Drawing::Point(12, 12);
 			this->btnMyCameraON->Name = L"btnMyCameraON";
 			this->btnMyCameraON->Size = System::Drawing::Size(105, 42);
 			this->btnMyCameraON->TabIndex = 0;
@@ -92,7 +98,7 @@ namespace HwaChat {
 			// 
 			// btnMyCameraOFF
 			// 
-			this->btnMyCameraOFF->Location = System::Drawing::Point(181, 105);
+			this->btnMyCameraOFF->Location = System::Drawing::Point(167, 12);
 			this->btnMyCameraOFF->Name = L"btnMyCameraOFF";
 			this->btnMyCameraOFF->Size = System::Drawing::Size(105, 42);
 			this->btnMyCameraOFF->TabIndex = 1;
@@ -100,79 +106,270 @@ namespace HwaChat {
 			this->btnMyCameraOFF->UseVisualStyleBackColor = true;
 			this->btnMyCameraOFF->Click += gcnew System::EventHandler(this, &MyForm::BtnMyCameraOFF_Click);
 			// 
+			// picCam
+			// 
+			this->picCam->Location = System::Drawing::Point(12, 103);
+			this->picCam->Name = L"picCam";
+			this->picCam->Size = System::Drawing::Size(475, 336);
+			this->picCam->TabIndex = 2;
+			this->picCam->TabStop = false;
+			// 
+			// btnPlay
+			// 
+			this->btnPlay->Location = System::Drawing::Point(502, 103);
+			this->btnPlay->Name = L"btnPlay";
+			this->btnPlay->Size = System::Drawing::Size(97, 32);
+			this->btnPlay->TabIndex = 3;
+			this->btnPlay->Text = L"Start";
+			this->btnPlay->UseVisualStyleBackColor = true;
+			this->btnPlay->Click += gcnew System::EventHandler(this, &MyForm::BtnPlay_Click);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(325, 251);
+			this->ClientSize = System::Drawing::Size(611, 451);
+			this->Controls->Add(this->btnPlay);
+			this->Controls->Add(this->picCam);
 			this->Controls->Add(this->btnMyCameraOFF);
 			this->Controls->Add(this->btnMyCameraON);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picCam))->EndInit();
 			this->ResumeLayout(false);
 
 		}
+		int showMyCamera()
+		{
+			camera abc;
+			Mat frame;
+			VideoCapture cam;
+
+			VideoCapture cap;
+			int deviceID = 0;             // 0 = open default camera
+			int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+			cap.open(deviceID + apiID);
+			if (!cap.isOpened()) {
+				cerr << "ERROR! Unable to open camera\n";
+				return -1;
+			}
+			cout << "Start grabbing" << endl
+				<< "Press any key to terminate" << endl;
+			for (;;)
+			{
+				cap.read(frame);
+				if (frame.empty()) {
+					cerr << "ERROR! blank frame grabbed\n";
+					break;
+				}
+
+				picCam->Image = abc.Show(frame);
+				if (waitKey(5) >= 0)
+					break;
+			}
+			return 0;
+		}
 	private: System::Void Button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		showMyCamera();
+		//showMyCamera();
 	}
 	private: System::Void BtnMyCameraOFF_Click(System::Object^ sender, System::EventArgs^ e) {
 		exit(0);
 	}
-};
-	int showMyCamera()
-	{
-		Mat frame;
-		VideoCapture cap;
+    private: System::Void BtnPlay_Click(System::Object^ sender, System::EventArgs^ e) {
+		showMyCamera();
+    }
+    };
+	//void detectAndDraw(Mat& img, CascadeClassifier& cascade,
+	//	CascadeClassifier& nestedCascade,
+	//	double scale, bool tryflip, Mat glasses)
+	//{
 
-		int deviceID = 0;
-		int apiID = cv::CAP_ANY;
+	//	Mat output2;
+	//	img.copyTo(output2);
 
-		cap.open(deviceID + apiID);
+	//	double t = 0;
+	//	vector<Rect> faces, faces2;
+	//	const static Scalar colors[] =
+	//	{
+	//		Scalar(255,0,0),
+	//		Scalar(255,128,0),
+	//		Scalar(255,255,0),
+	//		Scalar(0,255,0),
+	//		Scalar(0,128,255),
+	//		Scalar(0,255,255),
+	//		Scalar(0,0,255),
+	//		Scalar(255,0,255)
+	//	};
+	//	Mat gray, smallImg;
 
-		//CascadeClassifier face_classifier;
-		//face_classifier.load("C:\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml");
+	//	cvtColor(img, gray, COLOR_BGR2GRAY);
+	//	double fx = 1 / scale;
+	//	resize(gray, smallImg, cv::Size(), fx, fx, INTER_LINEAR_EXACT);
+	//	equalizeHist(smallImg, smallImg);
 
-		if (!cap.isOpened()) {
-			cerr << "ERROR! Unable to open camera\n";
-			return -1;
-		}
+	//	t = (double)getTickCount();
+	//	cascade.detectMultiScale(smallImg, faces,
+	//		1.1, 2, 0
+	//		//|CASCADE_FIND_BIGGEST_OBJECT
+	//		//|CASCADE_DO_ROUGH_SEARCH
+	//		| CASCADE_SCALE_IMAGE,
+	//		cv::Size(30, 30));
 
-		for (;;) 
-		{
-			//bool frame_valid = true;
-			cap.read(frame);
-			cap >> frame;
-			if (frame.empty()) {
-				cerr << "ERROR! blank frame grabbed\n";
-				//frame_valid = false;
-				break;
-			}
-			/*if (frame_valid) {
-				try {
-					Mat grayframe;
-					cvtColor(frame, grayframe, COLOR_BGR2GRAY);
-					equalizeHist(grayframe, grayframe);
+	//	t = (double)getTickCount() - t;
 
-					vector<Rect> faces;
+	//	Mat result;
 
-					face_classifier.detectMultiScale(grayframe, faces, 1.1, 3, CASCADE_FIND_BIGGEST_OBJECT | CASCADE_SCALE_IMAGE, cv::Size(30, 30));
+	//	printf("detection time = %g ms\n", t * 1000 / getTickFrequency());
+	//	for (size_t i = 0; i < faces.size(); i++)
+	//	{
+	//		Rect r = faces[i];
+	//		Mat smallImgROI;
+	//		vector<Rect> nestedObjects;
+	//		cv::Point center;
+	//		Scalar color = colors[i % 8];
+	//		int radius;
 
-					for (int i = 0; i < faces.size(); i++) {
-						cv::Point lb(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
-						cv::Point tr(faces[i].x, faces[i].y);
 
-						cv::rectangle(frame, lb, tr, cv::Scalar(0, 255, 0), 3, 4, 0);
-					}
 
-					imshow("Live", frame);
-				}catch(cv::Exception& e){
-					cerr << "Exception occurred. Ignoring frame... " << e.err << endl;
-				}
-			}*/
-			imshow("Live", frame);
-			if (waitKey(30) >= 0)
-				break;
-		}
-		return 0;
-	}
+	//		double aspect_ratio = (double)r.width / r.height;
+
+	//		if (0.75 < aspect_ratio && aspect_ratio < 1.3)
+	//		{
+	//			center.x = cvRound((r.x + r.width * 0.5) * scale);
+	//			center.y = cvRound((r.y + r.height * 0.5) * scale);
+	//			radius = cvRound((r.width + r.height) * 0.25 * scale);
+	//			circle(img, center, radius, color, 3, 8, 0);
+	//		}
+	//		else
+	//			rectangle(img, cv::Point(cvRound(r.x * scale), cvRound(r.y * scale)),
+	//				cv::Point(cvRound((r.x + r.width - 1) * scale), cvRound((r.y + r.height - 1) * scale)),
+	//				color, 3, 8, 0);
+	//		if (nestedCascade.empty()) {
+	//			cout << "nestedCascade.empty()" << endl;
+	//			continue;
+	//		}
+
+	//		smallImgROI = smallImg(r);
+	//		nestedCascade.detectMultiScale(smallImgROI, nestedObjects,
+	//			1.1, 2, 0
+	//			//|CASCADE_FIND_BIGGEST_OBJECT
+	//			//|CASCADE_DO_ROUGH_SEARCH
+	//			//|CASCADE_DO_CANNY_PRUNING
+	//			| CASCADE_SCALE_IMAGE,
+	//			cv::Size(20, 20));
+
+
+	//		cout << nestedObjects.size() << endl;
+
+	//		vector<cv::Point> points;
+
+	//		for (size_t j = 0; j < nestedObjects.size(); j++)
+	//		{
+	//			Rect nr = nestedObjects[j];
+	//			center.x = cvRound((r.x + nr.x + nr.width * 0.5) * scale);
+	//			center.y = cvRound((r.y + nr.y + nr.height * 0.5) * scale);
+	//			radius = cvRound((nr.width + nr.height) * 0.25 * scale);
+	//			circle(img, center, radius, color, 3, 8, 0);
+
+	//			cv::Point p(center.x, center.y);
+	//			points.push_back(p);
+	//		}
+
+
+	//		if (points.size() == 2) {
+
+	//			cv::Point center1 = points[0];
+	//			cv::Point center2 = points[1];
+
+	//			if (center1.x > center2.x) {
+	//				cv::Point temp;
+	//				temp = center1;
+	//				center1 = center2;
+	//				center2 = temp;
+	//			}
+
+
+	//			int width = abs(center2.x - center1.x);
+	//			int height = abs(center2.y - center1.y);
+
+	//			if (width > height) {
+
+	//				float imgScale = width / 330.0;
+
+	//				int w, h;
+	//				w = glasses.cols * imgScale;
+	//				h = glasses.rows * imgScale;
+
+	//				int offsetX = 150 * imgScale;
+	//				int offsetY = 160 * imgScale;
+
+	//				Mat resized_glasses;
+	//				resize(glasses, resized_glasses, cv::Size(w, h), 0, 0);
+
+	//				overlayImage(output2, resized_glasses, result, cv::Point(center1.x - offsetX, center1.y - offsetY));
+	//				output2 = result;
+	//			}
+	//		}
+	//	}
+
+	//	if (result.empty())
+	//		imshow("result", img);
+	//	else
+	//		imshow("result", result);
+
+	//}
+
+
+	//void overlayImage(const Mat & background, const Mat & foreground,
+	//	Mat & output, Point2i location)
+	//{
+	//	background.copyTo(output);
+
+
+	//	// start at the row indicated by location, or at row 0 if location.y is negative.
+	//	for (int y = std::max(location.y, 0); y < background.rows; ++y)
+	//	{
+	//		int fY = y - location.y; // because of the translation
+
+	//		// we are done of we have processed all rows of the foreground image.
+	//		if (fY >= foreground.rows) {
+	//			break;
+	//		}
+
+	//		// start at the column indicated by location, 
+
+	//		// or at column 0 if location.x is negative.
+	//		for (int x = std::max(location.x, 0); x < background.cols; ++x)
+	//		{
+	//			int fX = x - location.x; // because of the translation.
+
+	//									 // we are done with this row if the column is outside of the foreground image.
+	//			if (fX >= foreground.cols) {
+	//				break;
+	//			}
+
+	//			// determine the opacity of the foregrond pixel, using its fourth (alpha) channel.
+	//			double opacity =
+	//				((double)foreground.data[fY * foreground.step + fX * foreground.channels() + 3])
+
+	//				/ 255.;
+
+
+	//			// and now combine the background and foreground pixel, using the opacity, 
+
+	//			// but only if opacity > 0.
+	//			for (int c = 0; opacity > 0 && c < output.channels(); ++c)
+	//			{
+	//				unsigned char foregroundPx =
+	//					foreground.data[fY * foreground.step + fX * foreground.channels() + c];
+	//				unsigned char backgroundPx =
+	//					background.data[y * background.step + x * background.channels() + c];
+	//				output.data[y * output.step + output.channels() * x + c] =
+	//					backgroundPx * (1. - opacity) + foregroundPx * opacity;
+	//			}
+	//		}
+	//	}
+	//}
+
+
 }
