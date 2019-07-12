609320d9291077fa0f95b1b60b0296a39ac6b66a
Pangsu <wxzgks@gmail.com>
2019-07-12 14:59:12 +0900

    ClientMain과 ServerMain으로 함수를 분할하였음.

--- HwaChatConsole/main.cpp
+++ HwaChatConsole/main.cpp
@@ -6,13 +6,11 @@ using namespace std;
 
 void main()
 {
-	string IP ("10.10.14.63");
+	string IP ("10.10.14.51");
 	string PORT("65005");
 	thread t1(ClientMain, IP, PORT);
 	thread t2(ServerMain, PORT);
 	
 	t1.join();
 	t2.join();
-	//ClientMain("10.10.14.63", "65005");
-	//ServerMain("65005");
 }
